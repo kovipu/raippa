1637601d3dfb15d8dd4af47373cb9863b7efee94
Jesse Rauha <jesse.rauha@gmail.com>
2019-03-30 02:30:54 +0200

    Dashboard liver refresh

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,43 +1,8 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-
-const PageTitle = styled.h2`
-  padding: .9rem 1.5rem;
-`
-
-const RankingWrapper = styled.div`
-  padding: .9rem 1.5rem;
-  display: flex;
-  align-items: center;
-  background: white;
-  margin: 1rem;
-  border-radius: 8px;
-  box-shadow: 0px 3px 15px rgba(0,0,0,0.1);
-`
-const RankingIndex = styled.div`
-  font-weight: bold;
-  margin-right: 1.5rem;
-  width: 2rem;
-  text-align: center;
-`
-
-const RankingImage = styled.img`
-  margin-right: 1rem;
-  border-radius: 50%;
-  height: 40px;
-  width: 40px;
-`
-
-const RankingName = styled.div`
-  font-size: 1.25rem;
-  font-weight: 500;
-  width: 100%;
-`
-
-const RankingPoints = styled.div`
-  white-space: nowrap;
-  font-size: 1.25rem;
-`
+import openSocket from 'socket.io-client';
+import FlipMove from 'react-flip-move';
+let socket;
 
 class Dashboard extends Component {
 
@@ -48,7 +13,7 @@ class Dashboard extends Component {
       employees: []
     };
 
-    this.loadCustomers(1)
+    this.loadCustomers(0)
       .then((data) => {
         this.setState({
           employees: data
@@ -58,7 +23,31 @@ class Dashboard extends Component {
 
   loadCustomers = (shopId) => {
     return fetch(`https://rmarket-backend.herokuapp.com/employees/${shopId}`)
-      .then(res => res.json())
+      .then(res => res.json());
+  }
+
+  componentDidMount() {
+    socket = openSocket('https://rmarket-backend.herokuapp.com/0'); // TODO: Replace with actual shopId
+    
+    socket.on('refresh', (data) => {
+      console.log(data);
+
+      const d = this.state.employees.map(e => {
+        if (e.idx === data.employeeIdx) {
+          return { ...e, points: data.points };
+        } else {
+          return e;
+        }
+      });
+
+      this.setState({
+        employees: d.sort((e1, e2) => e2.points - e1.points)
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    socket.disconnect();
   }
 
   render() {
@@ -67,17 +56,58 @@ class Dashboard extends Component {
     return (
       <div className="Dashboard">
         <PageTitle>Worker rankings</PageTitle>
-        {employees.map((e, i) => (
-          <RankingWrapper key={e.idx}>
-            <RankingIndex>{i + 1}</RankingIndex>
-            <RankingImage src={e.icon} />
-            <RankingName>{e.firstName} {e.lastName}</RankingName>
-            <RankingPoints>{e.points}</RankingPoints>
-          </RankingWrapper>
-        ))}
+        <FlipMove>
+          {employees.map((e, i) => (
+            <RankingWrapper key={e.idx}>
+              <RankingIndex>{i + 1}</RankingIndex>
+              <RankingImage src={e.icon} />
+              <RankingName>{e.firstName} {e.lastName}</RankingName>
+              <RankingPoints>{e.points}</RankingPoints>
+            </RankingWrapper>
+          ))}
+        </FlipMove>
       </div>
     );
   }
 }
+
+const PageTitle = styled.h2`
+  padding: .9rem 1.5rem;
+`;
+
+const RankingWrapper = styled.div`
+  padding: .9rem 1.5rem;
+  display: flex;
+  align-items: center;
+  background: white;
+  margin: 1rem;
+  border-radius: 8px;
+  box-shadow: 0px 3px 15px rgba(0,0,0,0.1);
+`;
+
+const RankingIndex = styled.div`
+  font-weight: bold;
+  margin-right: 1.5rem;
+  width: 2rem;
+  text-align: center;
+`;
+
+const RankingImage = styled.img`
+  margin-right: 1rem;
+  border-radius: 50%;
+  height: 40px;
+  width: 40px;
+`;
+
+const RankingName = styled.div`
+  font-size: 1.25rem;
+  font-weight: 500;
+  width: 100%;
+`;
+
+const RankingPoints = styled.div`
+  white-space: nowrap;
+  font-size: 1.25rem;
+`;
 
 export default Dashboard;
