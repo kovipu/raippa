0c714f8dc8791e96a9863ff315f7316815d54253
Konsta Purtsi <kovipu@utu.fi>
2019-03-30 10:20:21 +0200

    Add boilerplate code for Bodium component

---
+++ src/dashboard/Bodium.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Bodium = (props) => {
+  return (
+    <BodiumWrapper>
+      <h1>Bodium goes here!</h1>
+    </BodiumWrapper>
+  );
+};
+
+const BodiumWrapper = styled.div`
+  text-align: center;
+  width: 100%;
+`;
+
+export default Bodium;
